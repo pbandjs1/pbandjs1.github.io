870ee08264e03487488d2d8701396c85c777222a
Nick E. <109779364+pbandjs1@users.noreply.github.com>
2025-10-28 13:01:58 -0700

    Move HTML to root and images and scripts to relevant folders

--- src/blog.ts
+++ src/blog.ts
@@ -12,17 +12,17 @@ export const blogs: Blog[] = [
         title: "The Office is amazing",
         date: "10/13/2025",
         description: "The Office TV Show is one of the best things ever created in the whole universe. I love it so much I watch it all the time. I live and breath the Office. It shows up in my dreams. I want to be Jim.",
-        image: "the_office.jpg",
+        image: "images/the_office.jpg",
         imageAlt: "office tv show boxset cover",
-        slug: "blogs/office.html"
+        slug: "office.html"
     },
     {
         title: "Mariners in 5",
         date: "10/13/2025",
         description: "Currently watching the Seattle Mariners go against the Toronto Blue Jays. I'm originally from the Seattle area, and watching them crush the Jays I truly believe they are gonna make it to the World Series.",
-        image: "mariners.png",
+        image: "images/mariners.png",
         imageAlt: "mariners logo",
-        slug: "blogs/mariners.html"
+        slug: "mariners.html"
     }
 ];
 
